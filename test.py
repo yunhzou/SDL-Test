d3436428cee834e3e6d2b819b1bacea11fce0299
Jackie <email@example.com>
2024-09-23 14:46:10 -0400

    s

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from flows import single_CV, single_DPV, Rinse, single_complexation, RunExp
 # Rinse()
 # single_complexation("jobfile.json")
 # Rinse()
-single_complexation()
+single_complexation("jobfile.json")
 #Rinse()
 #RunExp("jobfile.json")  
 #Rinse()
